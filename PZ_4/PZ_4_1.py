01fe5dcbbc97eb8e40dabc6593e6810fed9f6a8e
KpolllkaJlu666 <fomenkoelizaveta16@gmail.com>
2025-11-12 16:26:55 +0300

    Update PZ_4_1.py

--- PZ_4/PZ_4_1.py
+++ PZ_4/PZ_4_1.py
@@ -1,16 +1,18 @@
-#
-x = float(input())
-n = int(input())
+#№1 Сумма ряда: X - X^2/2 + X^3/3 - ... + (-1)^(N-1) * X^N / N (приближение ln(1+X), |X| < 1, N > 0)
 
-s = 0.0
-k = 1
-p = x        # x^k
-sgn = 1      # (-1)^(k-1)
+try:
+    x = float(input("Введите X (|X| < 1): "))
+    n = int(input("Введите N (целое > 0): "))
 
-while k <= n:
-    s += sgn * p / k
-    p *= x
-    sgn = -sgn
-    k += 1
+    if (-1 < x < 1) and (n > 0):
+        s = 0.0
+        term = x  # первый член: x
+        for k in range(1, n + 1):
+            s += term
+            term *= (-x) * (k / (k + 1))  # term_k -> term_{k+1}
+        print("Сумма первых N членов:", s)
+    else:
+        print("Неверный ввод: требуется |X| < 1 и целое N > 0.")
 
-print(s)
+except ValueError:
+    print("Неверный ввод: требуется |X| < 1 и целое N > 0.")
