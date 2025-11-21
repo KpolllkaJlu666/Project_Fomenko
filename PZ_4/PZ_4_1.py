d8f129aa6b4749ba0c85ca8211a531d465720bf2
KpolllkaJlu666 <fomenkoelizaveta16@gmail.com>
2025-11-21 20:07:44 +0300

    PZ_4_1 && PZ_4_2

---
+++ PZ_4/PZ_4_1.py
@@ -0,0 +1,17 @@
+#
+try:
+    x = float(input("Введите X (|X| < 1): "))
+    n = int(input("Введите N (целое > 0): "))
+
+    if (-1 < x < 1) and (n > 0):
+        s = 0.0
+        term = x  # первый член: x
+        for k in range(1, n + 1):
+            s += term
+            term *= (-x) * (k / (k + 1))  # term_k -> term_{k+1}
+        print("Сумма первых N членов:", s)
+    else:
+        print("Неверный ввод: требуется |X| < 1 и целое N > 0.")
+
+except ValueError:
+    print("Неверный ввод: требуется |X| < 1 и целое N > 0.")
